3bc34b1f22d7128ac071be3a5291d7bf162e3b4a
lysistrata1989 <ehamiltonhudson@gmail.com>
2018-10-22 20:33:20 -0400

    Done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,8 @@
-def my_select(collection)
- # your code here!
-end
+def my_select(array)
+   i = 0
+   results = []
+    while i < array.length
+      return results = array.select { |i| yield(i) }
+      i += 1
+    end
+  end
